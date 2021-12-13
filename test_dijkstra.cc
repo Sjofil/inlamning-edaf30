0e53e0b5101e2aeaafa8d85e3f0ef6b29f6ea2bd
OscarBli <58166848+OscarBli@users.noreply.github.com>
2021-12-13 19:58:19 +0100

    l

--- test_dijkstra.cc
+++ test_dijkstra.cc
@@ -3,6 +3,7 @@
 
 #include "dijkstra.h"
 #include "Node.h"
+#include "NodeSet.h"
 using std::cout;
 using std::endl;
 
@@ -46,9 +47,33 @@ void test()
 }
 
 void dijkstra(Node* start){
-    start->setValue(0);
 
-    
+    //Låt start vara noden vi vill räkna avstånd från. Sätt start:s värde till 0.
+    start->setValue(0);
+    NodeSet S{};
+
+    //Låt S vara en mängd av noder. Inledningsvis ska S innehålla en enda nod, nämligen start.
+    S.add(start);
+
+    //Så länge mängden S inte är tom ska följande upprepas:
+    while(!S.isEmpty()){
+        //Ta ut den nod ur S som har lägst värde. Kalla noden n
+        Node* n=S.removeMin();
+        //Gå igenom de bågar som utgår från n. För var och en av dessa bågar, gör följande:
+        for(Edge e:n->getEdges()){
+            //Kalla bågens längd för l och dess destination för d
+            int l=e.getLength();
+            Node* d=e.getDestination();
+            //Låt a vara summan av n:s värde och l.
+            int a=n->getValue()+l;
+            //Om a är mindre än d:s värde: ändra d:s värde till a, och lägg in d i mängden S.
+            if(a<d->getValue()){
+                d->setValue(a);
+                S.add(d);
+            }
+        }
+    }
+
 }
 
 int main()
